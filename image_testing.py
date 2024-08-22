86cd6549e491148094829dff362e34ec502ea0a9
tiaversa <timnaaversa@gmail.com>
2024-08-22 12:47:52 +0200

    add read me descriptions

--- image_testing.py
+++ image_testing.py
@@ -1,16 +1,17 @@
 import os, argparse
 from PIL import Image
 import numpy as np
+import pandas as pd
 
 parser = argparse.ArgumentParser(
     description=("Find the area with more white spaces in the image.")
 )
-parser.add_argument("--image_path", "-ip", help="Table to be transfered.",type=str, default="")
+parser.add_argument("--image_path", "-i", help="Name of the image.",type=str, default="")
 
 
 def test_image(image_name):
     # Load the image
-    img = Image.open(image_name)
+    img = Image.open(f'images/{image_name}')
 
     # Convert the image to grayscale
     gray_img = img.convert('L')
@@ -28,19 +29,21 @@ def test_image(image_name):
     # Determine the area with the most non-white pixels
     product_location = 'top' if np.mean(y_coordinates) > np_img.shape[0] / 2 else 'bottom'
 
+    return product_location
     # Place your text
-    if product_location == 'top':
-        print(f"Image: {image_name} has more open space on Top")
-        pass
-    else:
-        print(f"Image: {image_name} has more open space on Bottom")
-        pass
+    
 
 if __name__ == '__main__':
+    df = pd.DataFrame(columns=['image', 'section'])
     args = parser.parse_args()
     if not args.image_path == "":
-        test_image(args.image_path)
+        product_location = test_image(args.image_path)
+        print(f"Image {args.image_path} has more open space on {product_location}.")
+        df.loc[len(df.index)] = [args.image_path, product_location] 
     else:
         image_list = os.listdir('./images')
         for image in image_list:
-            test_image(f'images/{image}')
+            product_location = test_image(image)
+            print(f"Image {image} has more open space on {product_location}.")
+            df.loc[len(df.index)] = [image, product_location] 
+    df.to_csv('images_report.csv', index=False)
